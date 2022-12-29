c4d46a5b6e9d4b0770d583e30a033b5bb66b8997
Vesa Lukkarila <github@vesalukkarila.com>
2022-12-30 01:23:09 +0200

    feat: Initial csv parsing

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+type TLD struct {
+	Domain      string
+	Description string
+	Type        string
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	f, err := os.Open("data/country.csv")
+	defer f.Close()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	tlds, err := parseTlds(f)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(tlds)
+}
+
+func parseTlds(source io.Reader) ([]TLD, error) { //io.Reader = interface
+
+	var tlds []TLD
+	var err error
+
+	scanner := bufio.NewScanner(source) //study
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ";")
+
+		tld := TLD{
+			Domain:      parts[0],
+			Description: parts[1],
+			Type:        parts[2],
+		}
+
+		tlds = append(tlds, tld)
+	}
+
+	return tlds, err
 }
